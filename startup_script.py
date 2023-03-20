c9a122ccecd84038d441166434e390a38a9b5193
sstarzh <36517638+sstarzh@users.noreply.github.com>
2023-03-20 11:23:30 -0400

    Reworked script and added enabling rules

--- startup_script.py
+++ startup_script.py
@@ -33,16 +33,56 @@ logging.basicConfig(
 logging.info('-----------Initializing startup script-----------')
 time.sleep(60)
 
+def getApi(uri):
+    URI = uri
+    sender = Sender(credentials, URI, "GET", always_hash_content=False, ext=ORGANIZATION_ID )
+    response = requests.get(URI, headers={'Authorization': sender.request_header})
+    return response
+
+def putApi(uri,payload):
+    URI = uri
+    sender = Sender(credentials, URI, "PUT", always_hash_content=True, ext=ORGANIZATION_ID, content=json.dumps(payload), content_type="application/json" )
+    response = requests.put(URI, headers={'Authorization': sender.request_header, 'content-type': 'application/json'}, data=json.dumps(payload))
+    return response
+
+def postApi(uri,payload):
+    URI = uri
+    sender = Sender(credentials, URI, "POST", always_hash_content=True, ext=ORGANIZATION_ID, content=json.dumps(payload), content_type="application/json" )
+    response = requests.post(URI, headers={'Authorization': sender.request_header, 'content-type': 'application/json'}, data=json.dumps(payload))
+    return response
+
 logging.info('Getting UDF User Tags')
 r =requests.get('http://metadata.udf/userTags')
 r.raise_for_status()
 tags = r.json()
-ACCOUNT_ID = jq.compile(".userTags.name.ACCOUNT.value | keys[]").input(tags).first()
-USER_ID = jq.compile(".userTags.name.USER.value | keys[]").input(tags).first()
-ORGANIZATION_ID = jq.compile(".userTags.name.ORG.value | keys[]").input(tags).first()
-TS_DEPLOY_KEY = jq.compile(".userTags.name.DEPLOYMENT_KEY.value | keys[]").input(tags).first()
-API_KEY = jq.compile(".userTags.name.API_KEY.value | keys[]").input(tags).first()
-# Add additionalSetupConfig: ""
+def getTags(tagName):
+    try:
+        for key, value in tags.get('userTags').get('name').get(tagName).get('value').items():
+            if tagName == 'ENABLE_RULES' and value != None:
+                return True
+            else:
+                return key
+    except AttributeError:
+        if tagName != 'RULESET' and tagName != 'ENABLE_RULES':
+            logging.info('Tag %s is not defined. Exiting', tagName)
+            exit(1)
+        else:
+            if tagName == 'RULESET':
+                logging.info('No rulesets defined. Will enable rules in base, docker and k8s rulesets if "ENABLE_RULES" flag is set')
+                return None
+            else:
+                logging.info('Not enabling any rules')
+                return False
+                
+
+ACCOUNT_ID = getTags('ACCOUNT')
+USER_ID = getTags('USER')
+ORGANIZATION_ID = getTags('ORG')
+TS_DEPLOY_KEY = getTags('DEPLOYMENT_KEY')
+API_KEY = getTags('API_KEY')
+ENABLE = getTags('ENABLE_RULES')
+RULESET = getTags('RULESET')
+
 
 logging.info('AIP Deployment key: ' + TS_DEPLOY_KEY)
 values = requests.get('https://raw.githubusercontent.com/threatstack/threatstack-helm/master/values.yaml')
@@ -115,12 +155,14 @@ while not stack_exists('ts0-'+UUID):
 out = client.describe_stacks(StackName='ts0-'+UUID)
 outp = json.dumps(out, indent=4, sort_keys=True, default=str)
 outputs = json.loads(outp)
+
 logging.info('Populating AWS outputs')
 
 ARN = jq.compile('.Stacks[].Outputs[] | select(.OutputKey=="RoleARN").OutputValue').input(outputs).first()
 SQS = jq.compile('.Stacks[].Outputs[] | select(.OutputKey=="SQSSource").OutputValue').input(outputs).first()
 S3 = jq.compile('.Stacks[].Outputs[] | select(.OutputKey=="S3Bucket").OutputValue').input(outputs).first()
 REGION = jq.compile('.Stacks[].Outputs[] | select(.OutputKey=="CloudTrailRegion").OutputValue').input(outputs).first()
+
 HOST = "api.threatstack.com"
 BASE_PATH = 'https://' + HOST
 URI_PATH = '/v2/integrations/aws'
@@ -133,9 +175,7 @@ credentials = {
 URL = BASE_PATH + URI_PATH
 logging.info('Creating AWS Integration')
 payload1 = {'arn': ARN, 'description': 'UDF AWS CloudTrail integration'}
-sender1 = Sender(credentials, URL, "POST", always_hash_content=True, ext=ORGANIZATION_ID, content=json.dumps(payload1), content_type="application/json")
-response1 = requests.post(URL, headers={'Authorization': sender1.request_header, 'content-type': 'application/json'}, data=json.dumps(payload1))
-
+response1 = postApi(URL,payload1)
 ID = response1.json().get("id")
 with open('/var/tmp/int_id', 'w') as f:
     f.write(ID)
@@ -174,8 +214,7 @@ logging.info('Populating CloudTrail Params in AWS AIP Integration')
 
 URL_CLOUDTRAIL = URL + '/' + ID + '/cloudtrail'
 payload2 = {'source': SQS, 'region': REGION, 's3Bucket': S3}
-sender2 = Sender(credentials, URL_CLOUDTRAIL, "PUT", always_hash_content=True, ext=ORGANIZATION_ID, content=json.dumps(payload2), content_type="application/json" )
-response2 = requests.put(URL_CLOUDTRAIL, headers={'Authorization': sender2.request_header, 'content-type': 'application/json'}, data=json.dumps(payload2))
+response2 = putApi(URL_CLOUDTRAIL,payload2)
 if response2.status_code != 204:
         logging.info('Something went wrong...Aborting startup script. Status code:' + str(response2.status_code))
         exit(1)
@@ -183,35 +222,43 @@ else:
         logging.info('Successfully populated all params')
 
 #
-# Add rule enable API calls HERE
+# Enable rules defined in User Tag
 #
-"""
-RULES_URI = BASE_PATH + '/v2/rulesets'
-sender = Sender(credentials, RULES_URI, "GET", always_hash_content=True, ext=ORGANIZATION_ID )
-response = requests.put(RULES_URI, headers={'Authorization': sender.request_header})
-if response.status_code != 200:
-        logging.info('Something went wrong...Aborting startup script. Status code:' + str(response2.status_code))
-        exit(1)
-else:
-         for i in response['rulesets']:
-            if i['name'] == 'Base Rule Set':
-                base_ruleset = i['id']
-                base_rules = i['rules']
-                for r in base_rules:
-                    RULE_URI = BASE_PATH + '/v2/rulesets' + '/' + base_ruleset + '/rules/' + r
 
-
-            if i['name'] == 'Docker Rule Set':
-                docker_ruleset = i['id']
-                docker_rules = i['rules']
-            if i['name'] == 'Kubernetes Rule Set':
-                k8s_ruleset = i['id']
-                k8s_rules = i['rules']
-
-
-base, docker and k8s rulesets
-    
-"""
+if ENABLE:
+    if RULESET != None:
+        rulesets = RULESET
+    else:
+        rulesets = ['Base Rule Set','Docker Rule Set','Kubernetes Rule Set']
+    RULES_URI = BASE_PATH + '/v2/rulesets'
+    response = getApi(RULES_URI)
+    resp_json = response.json()
+    if response.status_code != 200:
+            logging.info('Something went wrong...Aborting startup script. Status code:' + str(response.status_code))
+            exit(1)
+    else:
+            for i in resp_json.get("rulesets"):
+                if rulesets.count(i.get("name")) == 1:
+                    base_ruleset = i.get("id")
+                    base_rules = i.get("rules")
+                    for r in base_rules:
+                        RULE_URI = BASE_PATH + '/v2/rulesets' + '/' + base_ruleset + '/rules/' + r
+                        response = getApi(RULE_URI)
+                        json_data = response.json()
+                        for item in json_data:
+                            for key, value in json_data.items():
+                                    if json_data["enabled"] == False:
+                                            json_data["enabled"] = True
+                    #    logging.info('JSON data: ' + str(json.dumps(json_data)))
+                        if response.status_code != 200:
+                            logging.info('Error retrieving the rule ' + r +'. Status code:' + str(response.status_code))
+                        else:
+                            payload = json_data
+                            response = putApi(RULE_URI,payload)
+                            if response.status_code != 200:
+                                logging.info('Error enabling the rule ' + r +'. Status code:' + str(response.status_code))
+                            else:
+                                logging.info('Successfully enabled the rule ' + r )
 
 
 logging.info('------------Startup script complete--------------')
